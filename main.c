1c5735530d93de003a1c6374c49779fb7f6b5102
Dario Hoyo <hoyodario@gmail.com>
2018-02-03 18:09:05 -0600

    Tarea terminada

--- main.c
+++ main.c
@@ -1,66 +1,160 @@
-/*
- * Copyright (c) 2017, NXP Semiconductor, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
- 
 /**
- * @file    LED_RGB.c
+ * @file    PushButton.c
  * @brief   Application entry point.
  */
 #include <stdio.h>
-#include "board.h"
-#include "peripherals.h"
-#include "pin_mux.h"
-#include "clock_config.h"
+#include "DataTypeDefinitions.h"
 #include "MK64F12.h"
-/* TODO: insert other include files here. */
+#define NUM_COLORES 5
 
-/* TODO: insert other definitions and declarations here. */
-
+void choose_color(int);
+void delay(uint16 delay);
 /*
  * @brief   Application entry point.
  */
 int main(void) {
+	/**Variable to capture the input value*/
+	uint32 inputValueS2 = 0;
+	uint32 inputValueS3 = 0;
+	uint8 color_c = 0;
 
-  	/* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
-  	/* Init FSL debug console. */
-    BOARD_InitDebugConsole();
+	/**Activating the GPIOB, GPIOC and GPIOE clock gating*/
+	SIM->SCGC5 = 0x2E00;
+	/**Pin control configuration of GPIOB pin22 and pin21 as GPIO*/
+	PORTB->PCR[21] = 0x00000100;
+	PORTB->PCR[22] = 0x00000100;
+	/**Pin control configuration of GPIOC pin6 as GPIO with is pull-up resistor enabled*/
+	PORTC->PCR[6] = 0x00000103;
+	PORTA->PCR[4] = 0x00000103;
+	/**Pin control configuration of GPIOE pin26 as GPIO*/
+	PORTE->PCR[26] = 0x00000100;
+	/**Assigns a safe value to the output pin21 of the GPIOB*/
+	GPIOB->PDOR = 0x00200000;
+	/**Assigns a safe value to the output pin22 of the GPIOB*/
+	GPIOB->PDOR |= 0x00400000;
+	/**Assigns a safe value to the output pin26 of the GPIOE*/
+	GPIOE->PDOR |= 0x04000000;
 
-    printf("Hello World\n");
+	GPIOC->PDDR &=~(0x40);
+	GPIOA->PDDR &=~(0x10);
+	/**Configures GPIOB pin21 as output*/
+	GPIOB->PDDR = 0x00200000;
+	/**Configures GPIOB pin22 as output*/
+	GPIOB->PDDR |= 0x00400000;
+	/**Configures GPIOE pin26 as output*/
+	GPIOE->PDDR |= 0x04000000;
 
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
+    while(1)
+    {
+    	//copia valor en sw2 Y sw3
+		inputValueS2 = GPIOC->PDIR;
+		inputValueS3 = GPIOA->PDIR;
+		/**Masks the GPIOC in the bit of interest*/
+		inputValueS2 = inputValueS2 & 0x40;
+		inputValueS3 = inputValueS3 & 0x10;
+		/**Note that the comparison is not inputValur == False, because it is safer if we switch the arguments*/
+
+		if(FALSE == inputValueS2)
+		{
+			color_c++;
+
+			choose_color(color_c%NUM_COLORES);
+
+			while(FALSE == inputValueS2)
+			{
+				inputValueS2 = GPIOC->PDIR;
+				inputValueS3 = GPIOA->PDIR;
+				/**Masks the GPIOC in the bit of interest*/
+				inputValueS2 = inputValueS2 & 0x40;
+				inputValueS3 = inputValueS3 & 0x10;
+				if((FALSE == inputValueS2) && (FALSE == inputValueS3))
+				{
+					color_c--;
+					GPIOB->PDOR &= ~(0x00400000);/**Read led on*/
+					GPIOB->PDOR &= ~(0x00200000);/**Blue led on*/
+					GPIOE->PDOR &= ~(0x4000000);/**Green led on*/
+
+					while((FALSE == inputValueS3))
+					{
+						inputValueS3 = GPIOA->PDIR;
+						inputValueS3 = inputValueS3 & 0x10;
+					}
+				}
+			}
+		}
+
+		else if(FALSE == inputValueS3)
+		{
+			color_c--;
+
+			choose_color(color_c%NUM_COLORES);
+
+			while(FALSE == inputValueS3)
+			{
+				inputValueS2 = GPIOC->PDIR;
+				inputValueS3 = GPIOA->PDIR;
+				/**Masks the GPIOC in the bit of interest*/
+				inputValueS2 = inputValueS2 & 0x40;
+				inputValueS3 = inputValueS3 & 0x10;
+				if((FALSE == inputValueS2) && (FALSE == inputValueS3))
+				{
+					color_c++;
+					GPIOB->PDOR &= ~(0x00400000);/**Read led on*/
+					GPIOB->PDOR &= ~(0x00200000);/**Blue led on*/
+					GPIOE->PDOR &= ~(0x4000000);/**Green led on*/
+
+					while((FALSE == inputValueS2))
+					{
+
+						inputValueS2 = GPIOC->PDIR;
+						inputValueS2 = inputValueS2 & 0x40;
+					}
+				}
+			}
+		}
     }
     return 0 ;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// EOF
+////////////////////////////////////////////////////////////////////////////////
+void delay(uint16 delay)
+{
+	volatile uint16 counter;
+
+	for(counter=delay; counter > 0; counter--)
+	{
+	}
+}
+
+void choose_color(int color){
+	switch(color)
+				{
+				case 0:
+					GPIOB->PDOR |= 0x00200000;/**Blue led off*/
+					GPIOB->PDOR |= 0x00400000;/**Read led off*/
+					GPIOE->PDOR &= ~(0x4000000);
+				break;
+				case 1:
+					GPIOB->PDOR |= 0x00400000;/**Read led off*/
+					GPIOE->PDOR |= 0x4000000;/**Green led off*/
+					GPIOB->PDOR &= ~(0x00200000);/**Blue led on*/
+				break;
+				case 2:
+					GPIOE->PDOR |= 0x4000000;/**Green led off*/
+					GPIOB->PDOR &= ~(0x00400000);/**Read led on*/
+					GPIOB->PDOR &= ~(0x00200000);/**Blue led on*/
+				break;
+				case 3:
+					GPIOB->PDOR &= ~(0x00400000);/**Read led on*/
+					GPIOB->PDOR |= 0x00200000;/**Blue led off*/
+					GPIOE->PDOR |= 0x4000000;/**Green led off*/
+				break;
+				case 4:
+					GPIOB->PDOR &= ~(0x00400000);/**Read led on*/
+					GPIOE->PDOR &= ~(0x4000000);/**Green led on*/
+					GPIOB->PDOR |= 0x00200000;/**Blue led off*/
+				break;
+				}
 }
